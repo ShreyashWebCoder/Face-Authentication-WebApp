ca7d781ed6d38b4d599c76d397a869c7b439fb74
Shreyash Web Developer <131770664+ShreyashWebCoder@users.noreply.github.com>
2024-02-08 22:08:32 +0530

    Add files via upload

---
+++ Mini-Project/src/Login/script-page.js
@@ -0,0 +1,3 @@
+function backClick(){
+    window.open("file:///C:/Users/shrey/OneDrive/Desktop/Mini-Project/face.html");
+}
---
+++ Mini-Project/src/Sign Up/script.js
@@ -0,0 +1,3 @@
+function moveClick(){
+    window.open("file:///C:/Users/shrey/OneDrive/Desktop/Mini-Project/src/Login/login-page.html");
+}
